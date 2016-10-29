27adf7337f00e6ac3da2df02a719cec4699a5d5a
MaikonSilva <maikon.silva@hotmail.com.br>
2016-10-29 01:25:08 -0300

    Coreções

---
+++ Zoofruit/Biblioteca/dados/iDAOAlimento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.basica;
+
+namespace Biblioteca.dados
+{
+    interface iDAOAlimento
+    {
+        void adicionar(Alimento a);
+        void alterar(Alimento a);
+        void excluir(Alimento a);
+        List<Alimento> pesquisar(Alimento a);
+    }
+}
